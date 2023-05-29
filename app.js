5d9c6d8735120cb3c107cacd484427deecd05dbc
grace kurui <gracekurui123@gmail.com>
2023-05-29 14:34:52 +0300

    simple sign up

---
+++ app.js
@@ -0,0 +1,7 @@
+// let town = {
+//   name: "eldoret",
+//   population: 4000000,
+//   way: function (name1, city) {
+//     console.log(`my name is ${name1} and my city is ${city}`);
+//   },
+// };
